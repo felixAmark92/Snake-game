ac8b1c915c0743f73ffd2fa81eb5772caf29e0ca
felixAmark92 <felix.amark@gmail.com>
2023-09-15 00:59:18 +0200

    Add project files.

---
+++ ConsoleDrawer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_game;
+
+internal class ConsoleDrawer
+{
+    public char[,] Box { get; private set; }
+
+    public ConsoleDrawer(char[,] box) 
+    {
+        Box = box;
+    }
+    public void UpdateScreenAt(Point point, char c)
+    {
+        Console.CursorLeft = point.X;
+        Console.CursorTop = point.Y;
+        Console.Write(c);
+        Box[point.Y, point.X] = c;
+    }
+    public void UpdateScreenAt(Point point, char c, ConsoleColor color)
+    {
+        Console.CursorLeft = point.X;
+        Console.CursorTop = point.Y;
+        Console.ForegroundColor = color;
+        Console.Write(c);
+        Console.ResetColor();
+        Box[point.Y, point.X] = c;
+    }
+
+    public void Draw()
+    {
+        for (int i = 0; i < Box.GetLength(0); i++)
+        {
+            for (int j = 0; j < Box.GetLength(1); j++)
+            {
+                Console.Write(Box[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public bool PointContains(Point point, char c)
+    {
+        return (Box[point.Y, point.X] == c);
+    }
+    public bool PointContains(Point point, char c, char c2)
+    {
+        return (Box[point.Y, point.X] == c || Box[point.Y, point.X] == c2);
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_game;
+
+//Point class because stupid Point in system.drawing is a struct and is causing problem
+internal class Point
+{
+    public int X {  get; set; }
+    public int Y { get; set; }
+
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point Copy()
+    {
+        return new Point(X, Y);
+    }
+}
