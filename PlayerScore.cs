7233c51f43ec65287b8f1bdeb3f0e691f121df7c
felixAmark92 <felix.amark@gmail.com>
2023-09-15 14:53:03 +0200

    Started building leaderboard

---
+++ PlayerScore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_game;
+
+internal record PlayerScore
+{
+    public long Score { get; set; }
+    public string PlayerName { get; set; } = string.Empty;
+
+    public PlayerScore(long score, string playerName)
+    {
+        Score = score;
+        PlayerName = playerName;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
 ﻿using FelixLibrary;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 using Snake_game;
 
 internal static class Program
 {
+    const string LEADERBOARD = "leaderboard.json";
     private static void Main(string[] args)
     {
+
         int selection;
         long score;
         var mainMenu = new MenuSelection();
+        SortedList<long, PlayerScore>? leaderboard;
+
         mainMenu.Add("Play");
         mainMenu.Add("Leaderboard");
         mainMenu.Add("Quit");
+
+
+        if (File.Exists(LEADERBOARD))
+        {
+            string jsonText = File.ReadAllText(LEADERBOARD);
+            leaderboard = JsonSerializer.Deserialize<SortedList<long, PlayerScore>>(jsonText);
+        }
+        else
+        {
+            leaderboard = new SortedList<long, PlayerScore>();
+        }
 
         
         while (true)
@@ -19,7 +36,28 @@ internal static class Program
             selection = mainMenu.RunMenuSelection();
 
             if (selection == 0)
-                Game.Run();
+            {
+                score = Game.Run();
+
+                if (leaderboard.Count < 10 || score > leaderboard.Last().Value.Score)
+                {
+                    Console.WriteLine("Enter your player name");
+                    var playerScore = new PlayerScore(score, Console.ReadLine());
+
+                    leaderboard.Add(score, playerScore);
+                    if (leaderboard.Count > 10)
+                    {
+                        leaderboard.Remove(leaderboard.Last().Key);
+                    }
+                    string jsonfile = JsonSerializer.Serialize(leaderboard);
+                    File.WriteAllText(LEADERBOARD, jsonfile);
+
+
+                }
+
+                
+
+            }
 
             if (selection == 1)
                 continue;
